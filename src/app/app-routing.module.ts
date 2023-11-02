539e4dab8b2067ef8af72bec99b26732f2f7bd9b
durga0702 <durga@sparkouttech.com>
2023-11-02 09:28:08 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { UserComponent } from './user/user.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashboard', component:UserDashboardComponent},
+  {path:'', redirectTo:'dashboard', pathMatch:'full'},
+  {path:'add-user', component:UserComponent},
+  {path:'update-user', component:UpdateUserComponent},
+  {path:'list-user', component:ListUserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { UserComponent } from './user/user.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import {MatTableModule} from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card'; 
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserDashboardComponent,
+    UpdateUserComponent,
+    ListUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
